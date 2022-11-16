1da8661bf000e5a40f00a4a3b44a8967531d59a6
andreadldev <andrea.delisi2@gmail.com>
2022-11-16 15:40:38 +0100

    Bottone che fa partire lo script

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 const { createApp } = Vue
 
 createApp({
-  data() {
-    return {
-        mailList: []
-    }
-  },
-  created() {
-    for (let i = 1; i <= 10; i++) {
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then((element) => {
-            let mail = element.data.response;
-            this.mailList.push(mail);
-        })
-    }
+    data() {
+        return {
+            mailList: []
+        }
+    },
+    methods: {
+        generateMail() {
+            this.mailList = [];
+            for (let i = 1; i <= 10; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((element) => {
+                    let mail = element.data.response;
+                    this.mailList.push(mail);
+                })
+            }
+        }
     }
 }).mount('#app')
