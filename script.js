1a982472479991e70faf9a98643e17612e9acda7
andreadldev <andrea.delisi2@gmail.com>
2022-11-16 15:25:03 +0100

    Stampa in pagina 10 email generate

--- script.js
+++ script.js
@@ -3,7 +3,16 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-
+        mailList: []
     }
-  }
+  },
+  created() {
+    for (let i = 1; i <= 10; i++) {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((element) => {
+            let mail = element.data.response;
+            this.mailList.push(mail);
+        })
+    }
+    }
 }).mount('#app')
